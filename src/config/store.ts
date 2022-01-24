1a0adc0f22ad3fd73f76398ef81c9498d640133c
Gmoreno100 <gmoreno@ithreexglobal.com>
2022-01-24 16:26:37 -0300

    feat(remove things)

--- src/config/store.ts
+++
@@ -1,9 +0,0 @@
-export const StoreOptions = {
-  secret: 'IhtReexGl0b4al2021',
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    maxAge: 3600000,
-    originalMaxAge: 3600000,
-  },
-};
